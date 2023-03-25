46bba158ceea3910c1f74bd30b1f37125033fada
MeghashreeLasya <128039615+MeghashreeLasya@users.noreply.github.com>
2023-03-25 22:26:00 +0530

    Update Activity8.java

--- Selenium/Project/Activity8.java
+++ Selenium/Project/Activity8.java
@@ -15,8 +15,8 @@ public class Activity8 {
         driver.get("https://alchemy.hguy.co/lms/");
         Thread.sleep(2000);
         driver.findElement(By.xpath("//a[text()='Contact']")).click();
-        driver.findElement(By.xpath("//input[@id='wpforms-8-field_0']")).sendKeys("Hemalatha");
-        driver.findElement(By.xpath("//input[@id='wpforms-8-field_1']")).sendKeys("hema.1205@gmail.com");
+        driver.findElement(By.xpath("//input[@id='wpforms-8-field_0']")).sendKeys("Meghashree");
+        driver.findElement(By.xpath("//input[@id='wpforms-8-field_1']")).sendKeys("meghamanjunath1@gmail.com");
         driver.findElement(By.xpath("//input[@id='wpforms-8-field_3']")).sendKeys("Testing the subject");
         driver.findElement(By.xpath("//textarea[@id='wpforms-8-field_2']")).sendKeys("Testing the comment section");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
